940d097c034c23057757d0b52848b468743644a8
Jason R. McNeil <jason@mcneil.dev>
2024-02-22 10:54:24 -0700

    nix: update flakes

--- nix/devShells/jasonrm-elon.nix
+++ nix/devShells/jasonrm-elon.nix
@@ -13,6 +13,7 @@
 
     arcanum.defaultRecipients = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII5gspOWcPeO/Qol7NbmvKIN8iQtGBYqhtPWwJMLSpYo jasonrm@elon"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ3k6zKT97l8vlxcmH5hekHEvnSDXpL6j8FFW/ZL3CXT jasonrm@raskin"
     ];
   };
 }
